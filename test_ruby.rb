c449a911d654739120f1a5af48522a889ec57dd4
hj <969070831@qq.com>
2019-06-19 22:28:20 +0800

    change

--- test_ruby.rb
+++ test_ruby.rb
@@ -1,2 +1,31 @@
+=begin
+对ruby进行学习
+基于ruby4.0版
 #encoding:UTF-8
-print "你好"
+print "你好\n"
+puts "first name #{ARGV[0]}"
+puts "second name #{ARGV[1]}"
+
+#文件操作
+filename=ARGV[0]
+file=File.open(filename)
+text= file.read
+puts text
+file.close
+text1=File.read(filename);
+puts text1
+#逐行
+filename=ARGV[0]
+file=File.open(filename)
+file.each_line do |line|
+puts line
+end
+=end
+pattern =Regexp.new(ARGV[0])
+filename=ARGV[1]
+file=File.open(filename)
+file.each_line do |line|
+if pattern=~line
+puts line
+end
+end
