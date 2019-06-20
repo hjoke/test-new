fd9a02584c0612ea6a0db86829d01ae9d06c427f
hj <969070831@qq.com>
2019-06-20 20:55:50 +0800

    uodata

--- test_ruby.rb
+++ test_ruby.rb
@@ -5,7 +5,7 @@
 print "你好\n"
 puts "first name #{ARGV[0]}"
 puts "second name #{ARGV[1]}"
-
+#irb --simple-prompt
 #文件操作
 filename=ARGV[0]
 file=File.open(filename)
@@ -20,7 +20,7 @@ file=File.open(filename)
 file.each_line do |line|
 puts line
 end
-=end
+
 pattern =Regexp.new(ARGV[0])
 filename=ARGV[1]
 file=File.open(filename)
@@ -29,3 +29,17 @@ if pattern=~line
 puts line
 end
 end
+#
+=end
+array=[0,1,2,3,4,5,6,9,10]
+array.each do |i|
+case i
+when 1,3,5 #then
+puts "#{i}是奇数"
+when 2,4,6
+puts "#{i}是偶数"
+else 
+puts "#{i}未出现"
+end
+end
+
